80f326a6d6105cfbfb5ef60c7157f41c7a3755a0
Karina Valiaeva <karinaval1989@yandex.ru>
2021-06-08 16:34:57 +0300

    fixed readme

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimiter');
 const { MONGO_URL } = require('./config');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3005 } = process.env;
 
 const app = express();
 
